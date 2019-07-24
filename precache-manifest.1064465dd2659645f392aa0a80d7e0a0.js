9b38cfa363cb3634d52f869a17cd324fe2962f9a
vinirg22 <aramburo.irving@gmail.com>
2019-07-24 14:31:26 -0700

    Updates

---
+++ precache-manifest.1064465dd2659645f392aa0a80d7e0a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fcfc107727f226c043d7b27726e78e2",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "6204c9b789d2e98b9486",
+    "url": "/clicky-game/static/css/main.8f0fa293.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "6204c9b789d2e98b9486",
+    "url": "/clicky-game/static/js/main.a3c71559.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
